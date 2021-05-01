e7a29ffa2a6269b717ffd3a2310e5c235c457283
alexiniguezj <80169043+alexiniguezj@users.noreply.github.com>
2021-05-01 15:35:38 -0500

    Add files via upload

---
+++ src/cliente.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleja
+ */
+import java.io.*;
+
+
+import java.net.*;
+
+
+class Cliente {
+
+
+ static final String HOST = "localhost";
+
+
+ static final int PUERTO=5000;
+
+
+ public Cliente( ) {
+
+
+   try{
+
+
+     Socket skCliente = new Socket( HOST , PUERTO );
+
+
+     InputStream aux = skCliente.getInputStream();
+
+
+     DataInputStream flujo = new DataInputStream( aux );
+
+
+     System.out.println( flujo.readUTF() );
+
+
+     skCliente.close();
+
+
+   } catch( Exception e ) {
+
+
+     System.out.println( e.getMessage() );
+
+
+   }
+
+
+ }
+
+
+ public static void main( String[] arg ) {
+
+
+   new Cliente();
+
+
+ }
+
+
+}
+
